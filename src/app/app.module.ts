b76508b8cd4835483d72eae5bcc79bceed93214f
Nikola Brežnjak <nikola.breznjak@gmail.com>
2017-11-06 13:11:41 +0100

    Finished project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,29 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { CalculatorPage } from '../pages/calculator/calculator';
 
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AdMobPro } from '@ionic-native/admob-pro';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    CalculatorPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    CalculatorPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        CalculatorPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        CalculatorPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        AdMobPro,
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,27 +1,77 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+import { AdMobPro } from '@ionic-native/admob-pro';
+import { Platform } from 'ionic-angular';
 
 @Component({
     selector: 'page-calculator',
     templateUrl: 'calculator.html'
 })
 export class CalculatorPage {
+    constructor(public navCtrl: NavController, private alertCtrl: AlertController, private admob: AdMobPro, private platform: Platform) {
+        this.platform.ready().then(() => {
+            var admobid = {
+                banner: 'ca-app-pub-7957971173858308/5068937357',
+                interstitial: 'ca-app-pub-7957971173858308/5667703151'
+            };
+
+            this.admob.createBanner({
+                adId: admobid.banner,
+                isTesting: true,
+                autoShow: true,
+                position: this.admob.AD_POSITION.BOTTOM_CENTER
+            })
+
+            this.admob.prepareInterstitial({
+                adId: admobid.interstitial,
+                isTesting: true,
+                autoShow: false
+            })
+        });
+    }
+
     result = '';
+    counter = 1;
+
+    showInterstitialAd() {
+        if (AdMobPro) {
+            this.admob.showInterstitial();
+        }
+    }
 
     btnClicked(btn) {
         if (btn == 'C') {
             this.result = '';
         }
         else if (btn == '=') {
-            this.result = eval(this.result);
+            if (this.result == '') {
+                return;
+            }
+
+            try {
+                this.result = eval(this.result).toFixed(2);
+
+                if (this.counter++ == 5) {
+                    this.showInterstitialAd();
+                    this.counter = 0;
+                }
+            } catch (error) {
+                this.showAlert();
+                this.result = '';
+            }
         }
         else {
             this.result += btn;
         }
     }
 
-    constructor(public navCtrl: NavController) {
-
+    showAlert() {
+        this.alertCtrl.create({
+            title: 'Malformed input',
+            subTitle: 'Ooops, please try again...',
+            buttons: ['Dismiss']
+        }).present();
     }
-
 }
